e13cc65b286d225cf285bef6e2d5ee78dfa39cf5
Danik Vitek <x3665107@gmail.com>
2022-02-03 14:12:49 +0200

    added ClientModInitializer impl

--- src/main/java/com/danikvitek/rocketboots/client/RocketBootsClient.java
+++ src/main/java/com/danikvitek/rocketboots/client/RocketBootsClient.java
@@ -1,5 +1,12 @@
 package com.danikvitek.rocketboots.client;
 
+import com.danikvitek.rocketboots.RocketBoots;
+import net.fabricmc.api.ClientModInitializer;
+
 @net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
-public class RocketBootsClient {
+public class RocketBootsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        RocketBoots.LOGGER.info("Client initialized");
+    }
 }
